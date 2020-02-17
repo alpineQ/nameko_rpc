25bdbca49d53f3d3560e6b7ca349e80956705357
JustAccount1702 <mkgolitsino@mail.ru>
2020-02-17 23:52:57 +0300

    Upgrade

--- client/client.py
+++ client/client.py
@@ -1,8 +1,30 @@
 from nameko.standalone.rpc import ClusterRpcProxy
+import pika
+from time import sleep
 
 config = {
-    'AMQP_URI': 'pyamqp://guest:guest@rabbitmq'
+    'AMQP_URI': 'amqp://guest:guest@rabbitmq'
 }
+
+
+def check_connection():
+    """try to establish connection and check its status"""
+    # set amqp credentials
+    parameters = pika.URLParameters(config['AMQP_URI'])
+    try:
+        connection = pika.BlockingConnection(parameters)
+        if connection.is_open:
+            print('OK')
+            connection.close()
+            return True
+    except Exception as error:
+        print('Error:', error.__class__.__name__)
+        return False
+
+
+while not check_connection():
+    print("Waiting...")
+    sleep(2)
 
 with ClusterRpcProxy(config) as rpc:
     print(rpc.greet_service.greet('Max K'))
